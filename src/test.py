a5383d54b704dbbdb82e863d08551fb84ad66ae8
Martin <gichurindungu@gmail.com>
2024-04-11 20:34:38 +0200

    add keras to reqs

--- src/test.py
+++ src/test.py
@@ -1,12 +1,2 @@
-from preprocessing import df 
-import numpy as np
-import pickle 
-
-
-try:
-    with open('./models/model.pkl', 'rb') as f:
-        model = pickle.load(f)
-    print("The file is a valid pickle file.")
-except Exception as e:
-    print("The file is not a valid pickle file. Error:", str(e))
- 
+import keras
+print(keras.__version__)
